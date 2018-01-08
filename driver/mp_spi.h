f5be49d1a356077e4f96b1e7124161319ea58162
stash-s <staszek.salik@gmail.com>
2018-01-08 08:20:27 +0100

    C/C++ interface cleanup.

--- driver/mp_spi.h
+++ driver/mp_spi.h
@@ -1,18 +1,18 @@
 /*
 * The MIT License (MIT)
-* 
+*
 * Copyright (c) 2015 David Ogilvy (MetalPhreak)
-* 
+*
 * Permission is hereby granted, free of charge, to any person obtaining a copy
 * of this software and associated documentation files (the "Software"), to deal
 * in the Software without restriction, including without limitation the rights
 * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 * copies of the Software, and to permit persons to whom the Software is
 * furnished to do so, subject to the following conditions:
-* 
+*
 * The above copyright notice and this permission notice shall be included in all
 * copies or substantial portions of the Software.
-* 
+*
 * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -51,8 +51,9 @@
 #define SPI_CLK_FREQ CPU_CLK_FREQ/(SPI_CLK_PREDIV*SPI_CLK_CNTDIV) // 80 / 20 = 4 MHz
 
 
-
-
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 void spi_init(uint8 spi_no);
 void spi_mode(uint8 spi_no, uint8 spi_cpha,uint8 spi_cpol);
@@ -75,5 +76,8 @@ uint32 spi_transaction(uint8 spi_no, uint8 cmd_bits, uint16 cmd_data, uint32 add
 #define spi_rx16(spi_no)      spi_transaction(spi_no, 0, 0, 0, 0, 0, 0, 16,   0)
 #define spi_rx32(spi_no)      spi_transaction(spi_no, 0, 0, 0, 0, 0, 0, 32,   0)
 
+#ifdef __cplusplus
+}
 #endif
 
+#endif
